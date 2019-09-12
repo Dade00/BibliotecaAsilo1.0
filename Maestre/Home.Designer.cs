e62b29c5852df2c20783ff8f10aff9dc7e3cca9b
DESKTOP-2CI9O8G\patty <zambecco.davide@gmail.com>
2019-09-12 16:33:24 +0200

    Maestre MOD

--- Maestre/Home.Designer.cs
+++ Maestre/Home.Designer.cs
@@ -31,7 +31,7 @@
             this.bambini_button = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.generi_button = new System.Windows.Forms.Button();
+            this.indietro_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // bambini_button
@@ -70,24 +70,24 @@
             this.button2.Text = "LIBRI";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // generi_button
+            // indietro_button
             // 
-            this.generi_button.FlatAppearance.BorderSize = 0;
-            this.generi_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.generi_button.Location = new System.Drawing.Point(366, 212);
-            this.generi_button.Margin = new System.Windows.Forms.Padding(2);
-            this.generi_button.Name = "generi_button";
-            this.generi_button.Size = new System.Drawing.Size(152, 57);
-            this.generi_button.TabIndex = 3;
-            this.generi_button.Text = "GENERI";
-            this.generi_button.UseVisualStyleBackColor = true;
+            this.indietro_button.FlatAppearance.BorderSize = 0;
+            this.indietro_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.indietro_button.Location = new System.Drawing.Point(366, 212);
+            this.indietro_button.Margin = new System.Windows.Forms.Padding(2);
+            this.indietro_button.Name = "indietro_button";
+            this.indietro_button.Size = new System.Drawing.Size(152, 57);
+            this.indietro_button.TabIndex = 3;
+            this.indietro_button.Text = "INDIETRO";
+            this.indietro_button.UseVisualStyleBackColor = true;
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1366, 768);
-            this.Controls.Add(this.generi_button);
+            this.Controls.Add(this.indietro_button);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.bambini_button);
@@ -105,7 +105,7 @@
         private System.Windows.Forms.Button bambini_button;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button generi_button;
+        private System.Windows.Forms.Button indietro_button;
     }
 }
 
