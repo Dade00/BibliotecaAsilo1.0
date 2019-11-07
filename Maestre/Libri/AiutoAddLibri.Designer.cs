e0182abbed1a6bfcb9cdbea70e73ff4195d694d2
Dade00 <zambecco.davide@gmail.com>
2019-11-07 01:21:33 +0100

    Aiuti

--- Maestre/Libri/AiutoAddLibri.Designer.cs
+++ Maestre/Libri/AiutoAddLibri.Designer.cs
@@ -35,22 +35,24 @@
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
+            this.label2.AutoEllipsis = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("GROBOLD", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(22, 118);
+            this.label2.Location = new System.Drawing.Point(38, 118);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(756, 78);
+            this.label2.Size = new System.Drawing.Size(725, 109);
             this.label2.TabIndex = 14;
-            this.label2.Text = "Inserire i dati dei libri negli appositi campi, dopo di che inserire la \r\nfoto tr" +
-    "amite apposito pulsante, una volta assicurati che tutti i \r\ncampi siano riempiti" +
-    " correttamente premere su \"CONFERMA\".";
+            this.label2.Text = "Inserire i dati dei libri negli appositi campi, dopo di che inserire la foto tram" +
+    "ite apposito pulsante, una volta assicurati che tutti i campi siano riempiti cor" +
+    "rettamente premere su \"CONFERMA\".";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("GROBOLD", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(321, 11);
+            this.label1.Location = new System.Drawing.Point(309, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(182, 58);
             this.label1.TabIndex = 13;
@@ -58,21 +60,26 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.BackgroundImage = global::Maestre.Properties.Resources.Ok;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(259, 340);
+            this.button1.Location = new System.Drawing.Point(248, 319);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(305, 100);
             this.button1.TabIndex = 12;
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // AiutoAddLibri
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Maestre.Properties.Resources.Sfondo_Aiuto;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
--- Maestre/Libri/AiutoDelLibri.Designer.cs
+++ Maestre/Libri/AiutoDelLibri.Designer.cs
@@ -36,11 +36,12 @@
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
+            this.label2.AutoEllipsis = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("GROBOLD", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(23, 123);
+            this.label2.Location = new System.Drawing.Point(36, 123);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(766, 78);
+            this.label2.Size = new System.Drawing.Size(721, 137);
             this.label2.TabIndex = 14;
             this.label2.Text = resources.GetString("label2.Text");
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -48,8 +49,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("GROBOLD", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(321, 11);
+            this.label1.Location = new System.Drawing.Point(309, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(182, 58);
             this.label1.TabIndex = 13;
@@ -57,21 +59,26 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.BackgroundImage = global::Maestre.Properties.Resources.Ok;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(259, 340);
+            this.button1.Location = new System.Drawing.Point(248, 316);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(305, 100);
             this.button1.TabIndex = 12;
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // AiutoDelLibri
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Maestre.Properties.Resources.Sfondo_Aiuto;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
