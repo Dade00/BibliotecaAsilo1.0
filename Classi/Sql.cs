de22321f44496696679f5034b302cee96df6c18f
DESKTOP-2CI9O8G\patty <zambecco.davide@gmail.com>
2019-11-08 01:41:43 +0100

    FINE

--- Classi/Sql.cs
+++ Classi/Sql.cs
@@ -10,7 +10,8 @@ namespace Classi
         private static SqlConnection DbInstance;
         public static SqlConnection getInstance()
         {
-            string ConnectionString = "data source = " + Credenziali.sqlServerAddress + ";initial catalog = " + Credenziali.sqlServerDB + ";persist security info=True;user id = " + Credenziali.sqlServerUser + ";password = " + Credenziali.sqlServerPassword + ";MultipleActiveResultSets=True";
+            XMLGesture xMLGesture = new XMLGesture();
+            string ConnectionString = "data source = " + xMLGesture.ServerAddress() + ";initial catalog = " + xMLGesture.initialCatalog() + ";persist security info=True;user id = " + xMLGesture.user() + ";password = " + xMLGesture.pw() + ";MultipleActiveResultSets=True";
             if (DbInstance == null)
             {
                 DbInstance = new SqlConnection(ConnectionString);
--- Classi/XMLGesture.cs
+++ Classi/XMLGesture.cs
@@ -8,32 +8,85 @@ namespace Classi
 {
     class XMLGesture
     {
-        private XmlTextReader reader()
+        private XmlTextReader inizializeReaderXML()
         {
             XmlTextReader reader = new XmlTextReader(@"C:\BibliotecaAsilo\SQL\Credenziali.xml");
+            return reader;
         }
 
         public string ServerAddress()
         {
             string server = "";
+            XmlTextReader reader = inizializeReaderXML();
+            int i = 0;
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Text && i==0)
+                {
+                    server = reader.Value;
+                    i++;
+                }
+                else if (reader.NodeType == XmlNodeType.Text)
+                    i++;
+            }
+            inizializeReaderXML().Close();
             return server;
         }
 
         public string initialCatalog()
         {
             string iCat = "";
+            XmlTextReader reader = inizializeReaderXML();
+            int i = 0;
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Text && i == 1)
+                {
+                    iCat = reader.Value;
+                    i++;
+                }
+                else if (reader.NodeType == XmlNodeType.Text)
+                    i++;
+            }
+            inizializeReaderXML().Close();
             return iCat;
         }
 
         public string user ()
         {
             string user = "";
+            XmlTextReader reader = inizializeReaderXML();
+            int i = 0;
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Text && i == 2)
+                {
+                    user = reader.Value;
+                    i++;
+                }
+                else if (reader.NodeType == XmlNodeType.Text)
+                    i++;
+            }
+            inizializeReaderXML().Close();
             return user;
         }
 
         public string pw()
         {
             string pw = "";
+            XmlTextReader reader = inizializeReaderXML();
+            int i = 0;
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Text && i == 3)
+                {
+                    pw = reader.Value;
+                    i++;
+                }
+                else if (reader.NodeType == XmlNodeType.Text)
+                    i++;
+            }
+            inizializeReaderXML().Close();
             return pw;
         }
 
